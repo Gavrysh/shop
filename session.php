f4b827df985ff23fe220d6cf11383cdb50921ec3
G@SeS <very.nice@ukr.net>
2018-07-06 16:45:03 +0300

    example 3. очистка данных сессии

--- session.php
+++ session.php
@@ -5,6 +5,7 @@ var_dump(session_name());
 
 var_dump(session_id());
 
+//Example 1, 2
 if (!isset($_SESSION['time'])) {
     $_SESSION['ua'] = $_SERVER['HTTP_USER_AGENT'];
     $_SESSION['time'] = date("H:i:s");
@@ -20,3 +21,14 @@ echo $_SESSION['time'];
  * Код JS который из консоли делает подмену переменной сессии
 javascript:(function(){document.cookie='PHPSESSID=5e2sroilm4578mos3q424cmbg2;path=/;';window.location.reload();})()
  */
+echo '<pre>';
+print_r($_SESSION);
+echo '</pre>';
+
+//Example 3
+unset($_SESSION['time']);
+echo '<pre>';
+print_r($_SESSION);
+echo '</pre>';
+
+$_SESSION = array();
