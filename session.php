9edde36a54f9705a28c8e523192ab919b16ff317
G@SeS <very.nice@ukr.net>
2018-07-06 16:05:40 +0300

    example 2 for session. protection from spoof session data.

--- session.php
+++ session.php
@@ -4,3 +4,14 @@ session_start();
 var_dump(session_name());
 
 var_dump(session_id());
+
+if (!isset($_SESSION['time'])) {
+    $_SESSION['ua'] = $_SERVER['HTTP_USER_AGENT'];
+    $_SESSION['time'] = date("H:i:s");
+}
+
+if ($_SESSION['ua'] != $_SERVER['HTTP_USER_AGENT']) {
+    die('Wrong browser');
+}
+
+echo $_SESSION['time'];
