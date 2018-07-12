95f4986ecf3a35cdf93f6e5e75b4d193c1843553
G@SeS <very.nice@ukr.net>
2018-07-12 17:14:12 +0300

    первоначальная работа по проектированию БД

--- index.php
+++ index.php
@@ -2,4 +2,41 @@
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
+<<<<<<< HEAD
 require_once './app/bootstrap.php';
+=======
+require_once  __DIR__.'/vendor/autoload.php';
+
+use App\DB;
+use Monolog\Logger;
+use Monolog\Handler\StreamHandler;
+
+if (empty($_GET)) {
+    $page = 'main';
+} else {
+    foreach ($_GET as $key => $val) {
+        preg_match('#^[a-z]+#s', $key, $m);
+        if ($m[0] == 'products') {
+            $page = 'products';
+        } elseif ($m[0] == 'product') {
+            $page = 'product';
+        } elseif ($m[0] == 'basket') {
+            $page = 'basket';
+        } elseif ($m[0] == 'calc') {
+            $page = 'calc';
+        } else {
+            $page = 'main';
+        }
+    }
+}
+
+include_once './model/'.$page.'.php';
+include_once './view/main.tpl';
+
+$var = new DB();
+$var;
+
+$var1 = new Monolog\Logger('name');
+$var1->pushHandler(new StreamHandler(__DIR__.'/shop.log', Logger::WARNING));
+$var1->warning('Foo');
+>>>>>>> RELEASE1
